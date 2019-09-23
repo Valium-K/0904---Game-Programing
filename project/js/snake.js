3a0a0d39283a5786230c9d718f6c57929097e31a
Valium-K <47807782+Valium-K@users.noreply.github.com>
2019-09-23 20:50:33 +0900

    클래스 세분화

--- project/js/snake.js
+++ project/js/snake.js
@@ -1,42 +1,96 @@
+
 /////////////////////////// 전역변수 ///////////////////////////////////////////////
 
+// MAP 상수
 const TILE_SIZE = 32; // Default: 32;
-const STROKE_SIZE = TILE_SIZE / 10;
-const TILE_NUM = 15;
-const MAP_SIZE = TILE_SIZE * TILE_NUM;
-var ITEM_SPAWN_RANDOM_NUM = 10;
-var isGameOver = false;
-var score = 0;
+const STROKE_SIZE = TILE_SIZE / 10; // 테두리 사이즈
+const TILE_NUM = 15; // 타일 수
+const MAP_SIZE = TILE_SIZE * TILE_NUM; // 맵 사이즈
+const MAP_COLOR = "#303840";
+const MAP_STROKE_COLOR = "#202830";
 
+// 지렁이 상수
+var SNAKE_SPEED = 250 * 0.5; // 지렁이 스피드
+const SNAKE_HEAD_COLOR = "#ff5900"; // 지렁이 머리 색상
+const SNAKE_TAIL_COLOR = "#ff9900"; // 지렁이 꼬리 색상
 
-const ITEM_VALUE = 3; // 아이템이 지울 꼬리 길이
-var tailOffset = 0; // 아이템으로 지운 꼬리길이 오프셋
-var itemCount = 0; // 아이템을 먹은 수 - 실제 꼬리 길이를 구하기 위함
+// 아이템 상수
+const ITEM_SPAWN_RANDOM_NUM = 10; // 아이템이 이 랜덤수와 같아야 스폰됨
 const LENGTH_FOR_ITEM_SPAWN = 10; // SNAKE가 해당 길이 이상일때 아이템 스폰됨
+const ITEM_VALUE = 3; // 아이템이 지울 꼬리 길이
+const ITEM_COLOR = "#8c00ff";
 
-var gameCanvas = document.getElementById("game_canvas");
-var gamemapContext = gameCanvas.getContext("2d");
-gameCanvas.width = MAP_SIZE;
-gameCanvas.height = MAP_SIZE;
+// FRUIT 상수
+const FRUIT_COLOR = "#99ff00";
 
+/////////////////////////////////////////////////////////////////////////////////
 
-
-const mapInit = function() {
-	// 맵 정보 init 및 draw
-	var map = document.getElementById("map");
-	var mapContxt = map.getContext("2d");
-	map.width = MAP_SIZE;
-	map.height = MAP_SIZE;
-
-	mapContxt.fillStyle = "#303840";
-	mapContxt.lineWidth = 5;
-	mapContxt.strokeStyle = "#202830";
+function Map() {
+	
+	this.map;
+	this.mapContxt;
+	
+	this.gameCanvas;
+	this.gamemapContext;
+	
+	// 맵 init
+	this.init = function() {
+		this.map = document.getElementById("map");
+		this.mapContxt = this.map.getContext("2d");
+		this.map.width = MAP_SIZE;
+		this.map.height = MAP_SIZE;
 		
-	for(var height = 0; height < MAP_SIZE / TILE_SIZE; height++) {
-		for(var width = 0; width < MAP_SIZE / TILE_SIZE; width++) {
-			mapContxt.fillRect(width*TILE_SIZE, height*TILE_SIZE, TILE_SIZE, TILE_SIZE);		
-			mapContxt.strokeRect(width*TILE_SIZE, height*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+		this.gameCanvas = document.getElementById("game_canvas");
+		this.gamemapContext = this.gameCanvas.getContext("2d");
+		this.gameCanvas.width = MAP_SIZE;
+		this.gameCanvas.height = MAP_SIZE;
+		this.gamemapContext.fillStyle = SNAKE_TAIL_COLOR;
+		
+		this.drawMap();
+	}
+	this.draw = function() {
+		this.drawFruit();
+		this.drawSnake();
+		this.drawItem();
+	}
+	this.drawMap = function() {
+		this.mapContxt.fillStyle = MAP_COLOR;
+		this.mapContxt.lineWidth = 5;
+		this.mapContxt.strokeStyle = MAP_STROKE_COLOR;
+		
+		for(var height = 0; height < MAP_SIZE / TILE_SIZE; height++) {
+			for(var width = 0; width < MAP_SIZE / TILE_SIZE; width++) {
+				this.mapContxt.fillRect(width*TILE_SIZE, height*TILE_SIZE, TILE_SIZE, TILE_SIZE);		
+				this.mapContxt.strokeRect(width*TILE_SIZE, height*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}
 		}
+	}
+	this.drawSnake = function() {
+				
+		// 꼬리 그리기
+		this.gamemapContext.fillStyle = SNAKE_TAIL_COLOR;
+		for(let i = snake.tailOffset; i < snake.tail.length; i++) {
+			this.gamemapContext.fillRect((snake.tail[i].x) + STROKE_SIZE, (snake.tail[i].y) + STROKE_SIZE, 
+										TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
+		}
+		
+		// 머리그리기
+		this.gamemapContext.fillStyle = SNAKE_HEAD_COLOR;
+		this.gamemapContext.fillRect(snake.getXPos() + STROKE_SIZE, snake.getYPos() + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
+	}
+	this.drawFruit = function() {
+		this.gamemapContext.fillStyle = FRUIT_COLOR;
+		this.gamemapContext.fillRect(fruit.getXPos() + STROKE_SIZE, fruit.getYPos() + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
+	}
+	this.drawItem = function() {
+		//console.log(isItemOnTheMap);
+		if(gameManager.isItemOnTheMap) {
+			this.gamemapContext.fillStyle = ITEM_COLOR;
+			this.gamemapContext.fillRect(gameManager.itemXPos + STROKE_SIZE, gameManager.itemYPos + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
+		}		
+	}
+	this.clearObject = function() {
+		this.gamemapContext.clearRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
 	}
 }
 
@@ -77,16 +131,18 @@ function Snake() {
 	// 플레이어 위치 - 타일 오프셋에 기반함
 	this.x = 0;
 	this.y = 0;
+	
 	// 타일 오프셋
 	this.xOffset = 0;
 	this.yOffset = 0;
+	
 	// 플레이어 속성
-	this.snakeSpeed = 250;
 	this.direction = '';
 	this.totalEatenFruit = 0;
-	gamemapContext.fillStyle = "#ff9900";
 
 	this.tail = [];
+	this.tailOffset = 0; // 아이템으로 지운 꼬리길이 오프셋
+	
 	// getter
 	this.getXPos = function() { return this.x; }
 	this.getYPos = function() { return this.y; }
@@ -94,32 +150,19 @@ function Snake() {
 	this.getYOffset = function() { return this.yOffset; }
 	this.getTotalEatenFruit = function() { return this.totalEatenFruit; }
 	this.getTail = function() { return this.tail; }
-	
+	this.getTailOffset = function() { return this.tailOffset; }
 	// setter
 	this.setXPos = function(x) {this.x = x;}
 	this.setYPos = function(y) {this.y = y;}
 	this.setXOffset = function(xo) {this.xOffset = xo;}
 	this.setYOffset = function(yo) {this.yOffset = yo;}
-	this.setTotalEatenFruit = function(fruitNum) { this.totalEatenFruit = fruitNum;}	
-
-	this.draw = function() {
-		
-		// 꼬리 그리기
-		gamemapContext.fillStyle = "#ff9900";
-		for(let i = tailOffset; i < this.tail.length; i++) {
-			gamemapContext.fillRect((this.tail[i].x) + STROKE_SIZE, (this.tail[i].y) + STROKE_SIZE, 
-										TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
-		}
-		
-		// 머리그리기
-		gamemapContext.fillStyle = "#ff5900";
-		gamemapContext.fillRect(this.x + STROKE_SIZE, this.y + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
-	}
+	this.setTotalEatenFruit = function(fruitNum) { this.totalEatenFruit = fruitNum; }	
+	this.setTailOffset = function(to) { this.tailOffset = to; }
 	
 	this.setPos = function() {
 		
 		// 꼬리 위치 한칸씩 이동 - 꼬리와 머리사이는 null 즉, tail[0]이 꼬리 끝, tail[length-1]이 머리바로 뒤 꼬리
-		for(let i = tailOffset; i < this.tail.length - 1; i++) {
+		for(let i = this.tailOffset; i < this.tail.length - 1; i++) {
 			this.tail[i] = this.tail[i + 1];
 		}
 		
@@ -164,14 +207,6 @@ function Snake() {
 				this.yOffset = 0;
 				this.direction = 'Right';
 				break;
-				
-			case 'e':
-					
-			fruit.pickLocation();
-			snake.totalEatenFruit++;
-			
-				
-			
 		}
 	}
 }
@@ -180,20 +215,25 @@ function Snake() {
 function Fruit() {
 	this.x;
 	this.y;
+
+	this.getXPos = function() { return this.x; }
+	this.getYPos = function() { return this.y; }
 	
+	this.setXPos = function(x) {this.x = x;}
+	this.setYPos = function(y) {this.y = y;}
+	
+	this.init = function() {
+		this.pickLocation();
+	}
 	this.pickLocation = function() {
 		this.x = (Math.floor(Math.random() * TILE_NUM - 1) + 1) * TILE_SIZE * 1;
 		this.y = (Math.floor(Math.random() * TILE_NUM - 1) + 1) * TILE_SIZE * 1;
 	}
 	
-	this.draw = function() {
-		gamemapContext.fillStyle = "#99ff00";
-		gamemapContext.fillRect(this.x + STROKE_SIZE, this.y + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
-	}
-	
 	this.fruitManager = function() {
 		// 과일 먹으면
-		if(snake.x == this.x && snake.y == this.y) {
+		if(snake.getXPos() == this.x && snake.getYPos() == this.y) {
+			
 			this.pickLocation();
 			snake.totalEatenFruit++;
 		}
@@ -201,44 +241,51 @@ function Fruit() {
 }
 
 function GameManagerClass() {
-	var itemSpawnNum = 0;
-	var itemXPos = 0;
-	var itemYPos = 0;
-	var isItemOnTheMap = false;
+	this.itemSpawnNum = 0;
+	this.itemXPos = null;
+	this.itemYPos = null;
+	this.isItemOnTheMap = false;
+	
+	this.isGameOver = false;
+	this.score = 0;
+	this.itemCount = 0; // 아이템을 먹은 수 - 실제 꼬리 길이를 구하기 위함
 	
 	this.init = function() {
 		this.itemSpawnNum = Math.floor(Math.random() * ITEM_SPAWN_RANDOM_NUM) + 1;
 	}
 	this.gameManagement = function() {
-		if(isGameOver) {
-			confirm("죽음");
+		if(this.isGameOver) {
+			confirm("Game Over! \nTotal Eaten Fruit: " + snake.totalEatenFruit);
 		}
 		
 		// 몸에 닿으면
 		for(let i = 0; i < (snake.getTail().length) - 1; i++) {
 			if(snake.getXPos() == snake.getTail()[i].x && snake.getYPos() == snake.getTail()[i].y) {
-				isGameOver = true;
+				this.isGameOver = true;
+				console.log("body Hit");
 			}
 		}
 		// 벽에 닿으면
 		if(snake.getXPos() < 0 || snake.getXPos() >= MAP_SIZE || snake.getYPos() < 0 || snake.getYPos() >= MAP_SIZE) {
-			isGameOver = true;
+			this.isGameOver = true;
+			console.log("wall Hit");
 		}
 		
 		// 아이템 스폰 조건 되면
 		if(this.itemSpawnNum == Math.floor(Math.random() * ITEM_SPAWN_RANDOM_NUM) + 1 // 랜덤수가 같고
-				&& snake.tail.length - (itemCount * ITEM_VALUE) >= LENGTH_FOR_ITEM_SPAWN // 실제 길이가 상수길이보다 크고
-				&& isItemOnTheMap == false) { // 맵에 아이템이 없으면
+				&& snake.getTail().length - (this.itemCount * ITEM_VALUE) >= LENGTH_FOR_ITEM_SPAWN // 실제 길이가 상수길이보다 크고
+				&& this.isItemOnTheMap == false) { // 맵에 아이템이 없으면
 			this.generateItem();
-			isItemOnTheMap = true;
+			this.isItemOnTheMap = true;
 		}
-		
+
 		// 아이템 먹으면 ITEM_VALUE 만큼 꼬리 길이 감소
 		if(snake.getXPos() == this.itemXPos && snake.getYPos() == this.itemYPos) {
-			isItemOnTheMap = false;
-			var temp = tailOffset + ITEM_VALUE;
+			this.isItemOnTheMap = false;
 			
-			for(var i = 0; i < temp; i++) {
+			var tailOffset = snake.getTailOffset() + ITEM_VALUE;
+			
+			for(var i = 0; i < tailOffset; i++) {
 				snake.tail[i] = {
 					x: -100,
 					y: -100
@@ -247,11 +294,9 @@ function GameManagerClass() {
 			this.itemXPos = -200;
 			this.itemYPos = -200;
 			
-			tailOffset = tailOffset + ITEM_VALUE;
-			itemCount++;
-		}
-		
-		
+			snake.setTailOffset(snake.getTailOffset() + ITEM_VALUE);
+			this.itemCount++;
+		}		
 	}
 	this.setItemSpawnNum = function() {
 		this.itemSpawnNum = Math.floor(Math.random() * ITEM_SPAWN_RANDOM_NUM) + 1;
@@ -265,49 +310,39 @@ function GameManagerClass() {
 	this.generateItem = function() {
 		this.setItemSpawnNum();
 		this.setRandomPos();
+	}
 
-	}
-	
-	this.drawItem = function() {
-		console.log(isItemOnTheMap);
-		if(isItemOnTheMap) {
-			gamemapContext.fillStyle = "#8c00ff";
-			gamemapContext.fillRect(this.itemXPos + STROKE_SIZE, this.itemYPos + STROKE_SIZE, TILE_SIZE - STROKE_SIZE*2, TILE_SIZE - STROKE_SIZE*2);
-		}		
-	}
 }
 
 (function init(){
-	mapInit();
+	
+	map = new Map();
+	map.init();
 	
 	// 입력 버퍼생성
 	queue = new Queue();
+	
 	// 플레이어 객체 생성
 	snake = new Snake();
-	snake.draw();
 	
 	fruit = new Fruit();
-	fruit.pickLocation();
-	fruit.draw();
+	fruit.init();
 	
 	gameManager = new GameManagerClass();
 	gameManager.init();
-	gameManager.gameManagement();
 	
+	map.draw();
 	
 	// 업데이트 함수
 	window.setInterval(() => {
-		
+		if(snake.totalEatenFruit >= 3) { SNAKE_SPEED = 50; }
 		snake.changeDirection();
-		gamemapContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height); 
-		fruit.draw();
+		map.clearObject();
 		snake.setPos();
-		snake.draw();
 		fruit.fruitManager();
 		gameManager.gameManagement();
-		gameManager.drawItem();
-		
-	}, snake.snakeSpeed * 0.5);
+		map.draw();
+	}, SNAKE_SPEED);
 }());
 
 
